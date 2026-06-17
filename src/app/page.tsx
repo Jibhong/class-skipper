471cd00d92174f4fb36dacc16805b1f738d837fd
BuildTools <pisitjibhong@gmail.com>
2026-06-18 00:18:50 +0700

    setup cloud run

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 "use client"
 
 import Calendar from "@/lib/client/components/calendar"
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    function test() {
+      console.log(process.env.NEXT_PUBLIC_A);
+      console.log(process.env.B);
+    }
+    test()
+  }, [])
+
   return (
     <main className="flex items-center justify-center h-screen">
       <Calendar />
