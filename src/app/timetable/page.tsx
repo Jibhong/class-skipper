b320086bee71fd7085f9bf1d6862bef81d7c34fa
mydKN <tuckjaru@gmail.com>
2026-06-27 22:15:47 +0700

    make timetable interactive and clickable

--- src/app/timetable/page.tsx
+++ src/app/timetable/page.tsx
@@ -1,20 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Timetable from "@/lib/client/components/Timetable";
 import { Header } from "@/lib/client/components/Components";
 
 export default function TimetablePage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-8 pt-24 bg-white">
-      <Header />
-      <Timetable />
-    </div>
-  );
-}
-
-export default function TimetablePage() {
-  return (
     <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Loading...</div>}>
-      <TimetableContent />
+      <div className="min-h-screen flex flex-col items-center justify-center p-8 pt-24 bg-white">
+        <Header />
+        <Timetable />
+      </div>
     </Suspense>
   );
 }
+
